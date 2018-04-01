a07eaa7fe7e3003a8d29d71653b2a55a3a2dc1e8
Amrit <s1443575@sms.ed.ac.uk>
2018-04-02 04:54:38 +0530

    Added Transmission delay

--- src/Link.java
+++ src/Link.java
@@ -1,15 +1,30 @@
-
+import java.util.Random;
 public class Link implements Runnable
 {
 	Message message;
+	Random rand;
 	
+	//delay ranges
+	int min,max;
+
 	public Link(Message message)
 	{
 		this.message = message;
+		rand = new Random(); 
+		
+		min = 900;
+		max = 1100;
+				
+	
 	}
 	public void run() {
 		//Delay in transmission
-		
+		int randomDelay = min + rand.nextInt(max -min +1);
+		try {
+			Thread.sleep(randomDelay);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		
 		message.receiver.receive(message);
 	}
